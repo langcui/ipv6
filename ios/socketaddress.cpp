1c225920c550322f81683cc40654d76ba00a6c35
cuiyuchun <cuiyuchun@yy.com>
2020-03-10 20:50:28 +0800

    init ios

---
+++ ios/socketaddress.cpp
@@ -0,0 +1,103 @@
+#include "socketaddress.h"
+void socketaddress::setInet(sockaddr_in addr4)
+{
+    mAddr.sin = addr4;
+}
+
+void socketaddress::setInet6(sockaddr_in6 addr6)
+{
+    mAddr.sin6 = addr6;
+}
+
+void socketaddress::setPort(uint16_t nport)
+{
+    if(mAddr.sa.sa_family == AF_INET)
+    {
+        mAddr.sin.sin_port = htons(nport);
+    }else if(mAddr.sa.sa_family == AF_INET6)
+    {
+        mAddr.sin6.sin6_port = htons(nport);
+    }
+}
+
+uint16_t socketaddress::getPort()
+{
+    if(mAddr.sa.sa_family == AF_INET)
+    {
+        return ntohs(mAddr.sin.sin_port);
+    }else if(mAddr.sa.sa_family == AF_INET6)
+    {
+        return ntohs(mAddr.sin6.sin6_port);
+    }
+    return 0;
+}
+
+sa_family_t socketaddress::getSockAddrFamily()
+{
+    return mAddr.sa.sa_family;
+}
+
+sockaddr* socketaddress::getSockAddr()
+{
+    return &mAddr.sa;
+}
+
+socklen_t socketaddress::getSockLen()
+{
+    socklen_t sl = 0;
+    if(mAddr.sa.sa_family == AF_INET)
+    {
+        sl = sizeof(sockaddr_in);
+    }
+    else if(mAddr.sa.sa_family == AF_INET6)
+    {
+        sl = sizeof(sockaddr_in6);
+    }
+    return sl;
+}
+
+std::string socketaddress::getIpStr()
+{
+    std::string resStr = "";
+    if(mAddr.sa.sa_family == AF_INET)
+    {
+        char addr1[INET_ADDRSTRLEN] = {'\0'};
+        ::inet_ntop(AF_INET,&(((sockaddr_in*)(&mAddr.sin))->sin_addr), addr1, sizeof(addr1));
+        resStr = std::string(addr1);
+    }
+    else if(mAddr.sa.sa_family == AF_INET6)
+    {
+        char addr2[INET6_ADDRSTRLEN] = {'\0'};
+        ::inet_ntop(AF_INET6,&(((sockaddr_in6*)(&mAddr.sin6))->sin6_addr), addr2, sizeof(addr2));
+        resStr = std::string(addr2);
+    }
+    return resStr;
+}
+
+std::string socketaddress::getPortStr()
+{
+    uint16_t hport = 0;// = htons(hport);
+    if(mAddr.sa.sa_family == AF_INET)
+    {
+        hport = ntohs(mAddr.sin.sin_port);
+    }
+    else if(mAddr.sa.sa_family == AF_INET6)
+    {
+        hport = ntohs(mAddr.sin6.sin6_port );
+    }
+    std::stringstream ss;
+    ss << hport;
+    return ss.str();
+}
+
+bool socketaddress::isIpv6(string ip)
+{
+    return ip.find(":") != string::npos;
+}
+
+
+
+
+
+
+
---
+++ ios/test_connect.cpp
@@ -0,0 +1,178 @@
+#include "socketaddress.h"
+
+int GetIpUnion(socketaddress &sockAddr, int domain, string sIp, int port)
+{
+
+    struct in6_addr ip;
+    char *addr1 = strdup(sIp.c_str());
+    inet_pton(AF_INET6, addr1, &ip);
+
+    struct sockaddr_in6 in6;
+    in6.sin6_addr  = ip;
+    in6.sin6_family = AF_INET6;
+    
+    sockAddr.setInet6(in6);
+    sockAddr.setPort(port);
+    
+
+    /*
+
+    sockaddrunion.sin6 = in6;
+
+    unsigned char buf[sizeof(struct in6_addr)];
+    char str[INET6_ADDRSTRLEN];
+    int s = inet_pton(domain, sIp.c_str(), buf);
+    if (s <= 0) {
+        if (s == 0)
+            fprintf(stderr, "Not in presentation format");
+        else
+            perror("inet_pton");
+        exit(EXIT_FAILURE);
+    }
+
+    if (inet_ntop(domain, buf, str, INET6_ADDRSTRLEN) == NULL) {
+        perror("inet_ntop");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("inet_ntop %s, buf[", str);
+    for(int i = 0; i < sizeof(struct in6_addr); ++i)
+    {
+        printf("%0x,", buf[i]);
+    }
+    printf("].\n");
+
+    sockaddr_in6 addr6;
+    printf("addr6.sin6_scope_id[%d].\n", addr6.sin6_scope_id);
+    addr6.sin6_len = sizeof(struct sockaddr_in6);
+    addr6.sin6_scope_id = 0;
+    addr6.sin6_family = AF_INET6;
+    addr6.sin6_flowinfo = 0;
+
+
+    memcpy(&addr6.sin6_addr, buf, sizeof(struct in6_addr));
+    sockAddr.setInet6(addr6);
+    sockAddr.setPort(port);
+    printf("init getIpStr[%s].\n", sockAddr.getIpStr().c_str());
+*/
+    /*
+
+       if(AF_INET == domain)
+       {
+       sockaddr_in addr4;
+       addr4.sin_family = AF_INET; 
+       memcpy(&addr4.sin_addr, buf, sizeof(struct in_addr));
+       sockAddr.setInet(addr4);
+       sockAddr.setPort(port);
+       }
+
+       if(AF_INET6 == domain)
+       {
+       sockaddr_in6 addr6;
+    //addr6.sin6_len = sizeof(struct sockaddr_in6);
+    printf("addr6.sin6_scope_id[%d].\n", addr6.sin6_scope_id);
+    addr6.sin6_len = sizeof(sockaddr_in6);
+    addr6.sin6_scope_id = 0;
+    addr6.sin6_family = AF_INET6;
+    addr6.sin6_flowinfo = 0;
+    memcpy(&addr6.sin6_addr, buf, sizeof(struct in6_addr));
+    sockAddr.setInet6(addr6);
+    sockAddr.setPort(port);
+    printf("init getIpStr[%s].\n", sockAddr.getIpStr().c_str());
+    }
+     */
+
+    return 0;
+}
+
+int TestConnect( socketaddress sockAddr)
+{
+    int family[] = {AF_INET6};
+    int sockettype[] = {SOCK_STREAM, SOCK_DGRAM};
+    // int sockettype[] = {SOCK_DGRAM, SOCK_STREAM};
+
+    for(int i = 0; i < sizeof(family)/sizeof(int); ++i)
+    {
+        for(int j = 0; j < 2; ++j)
+        {
+            errno = 0;
+            printf("------------\n");
+            printf("family[%d], sockaddr.family[%d], AF_INET[%d], AF_INET6[%d], AF_UNSPEC[%d].\n"
+                    , family[i], sockAddr.getSockAddrFamily(), AF_INET, AF_INET6, AF_UNSPEC);
+            printf("sockettype[%d], SOCK_STREAM[%d], SOCK_DGRAM[%d].\n", sockettype[j], SOCK_STREAM, SOCK_DGRAM);
+            int fd = socket(family[i], sockettype[j], 0);
+            printf("socket:fd[%d], errno[%d], strerror[%s].\n", 0, errno, strerror(errno));
+
+
+            signal(SIGPIPE, SIG_IGN);
+
+            char buf[100] = {0};
+            if(sockettype[j] == SOCK_STREAM)
+            {
+                int ret = connect(fd, sockAddr.getSockAddr(), sockAddr.getSockLen());
+                printf("connect getIpStr[%s].\n", sockAddr.getIpStr().c_str());
+                printf("connect ret[%d], socketlen[%d], errno[%d], strerror[%s].\n", ret, sockAddr.getSockLen(), errno, strerror(errno));
+                int size = send(fd, buf, 100, 0);
+                printf("send size[%d], errno[%d], strerror[%s].\n", size, errno, strerror(errno));
+            } else 
+            {
+                socketaddress mysocketaddress;
+                sockaddr_in6 myaddr6;
+                memset(&myaddr6, 0, sizeof(myaddr6));
+                //addr6.sin6_len = sizeof(struct sockaddr_in6);
+                printf("myaddr6.sin6_scope_id[%d].\n", myaddr6.sin6_scope_id);
+                myaddr6.sin6_len = sizeof(sockaddr_in6);
+                myaddr6.sin6_scope_id = 0;
+                myaddr6.sin6_family = AF_INET6;
+                myaddr6.sin6_flowinfo = 0;
+                myaddr6.sin6_addr = in6addr_any;
+
+                mysocketaddress.setInet6(myaddr6);
+                mysocketaddress.setPort(9989);
+
+                printf("before bind ip[%s], port[%s].\n", mysocketaddress.getIpStr().c_str(), mysocketaddress.getPortStr().c_str());
+                int ret = bind(fd, (const struct sockaddr *)mysocketaddress.getSockAddr(), mysocketaddress.getSockLen());
+                printf("bind ret[%d], socketlen[%d], errno[%d], strerror[%s].\n", ret, mysocketaddress.getSockLen(), errno, strerror(errno));
+                int size = sendto(fd, buf, 100, 0, sockAddr.getSockAddr(), sockAddr.getSockLen());
+                printf("sendto size[%d], errno[%d], strerror[%s].\n", size, errno, strerror(errno));
+            }
+
+            printf("------------\n");
+            close(fd);
+        }
+    }
+    return 0;
+}
+
+int main(int argc, char *argv[])
+{
+    socketaddress sockAddr;
+
+    int domain, s;
+    char str[INET6_ADDRSTRLEN];
+
+    if (argc != 3) {
+        fprintf(stderr, "Usage: %s ip port\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    string sIp = argv[1];
+    int port = atoi(argv[2]);
+    domain =  socketaddress::isIpv6(sIp) ? AF_INET6 : AF_INET;
+    //if(AF_INET == domain)
+    //   sIp = "::ffff:" + sIp;    
+    //    sIp = "64:ff9b::" + sIp;    
+
+    // domain = AF_INET6;
+    printf("input ip[%s], port[%d], domain[%d].\n", sIp.c_str(), port, domain);
+
+    GetIpUnion(sockAddr, domain, sIp, port);
+    printf("socketaddress:ai_family[%d], port[%d][%s], ip[%s].\n"
+            , sockAddr.getSockAddrFamily(), sockAddr.getPort(), sockAddr.getPortStr().c_str(), sockAddr.getIpStr().c_str());
+
+    TestConnect(sockAddr);
+
+    exit(EXIT_SUCCESS);
+}
+
+
